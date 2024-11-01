97df4a5afa86f552d5025469efad3fa80091df8b
Volodymyr Antoniuk <voxtobox@gmail.com>
2024-10-31 22:54:25 -0700

    init

--- src/1.ts
+++ src/1.ts
@@ -5,14 +5,12 @@
 */
 
 class Student {
-  public name: string;
-  public age: number;
-  public grade: string;
-
-  constructor(name: string, age: number, grade: string) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
+  constructor(
+    public name: string,
+    public age: number,
+    public grade: string
+  ) {
+    
   }
 }
 
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,64 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+// Клас Person
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+// Абстрактний клас House
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[];
+
+  constructor(key: Key) {
+    this.key = key;
+    this.door = false;
+    this.tenants = [];
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Людина увійшла в будинок");
+    } else {
+      console.log("Двері закриті");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+// Клас MyHouse
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Двері відкриті");
+    } else {
+      console.log("Неправильний ключ");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +67,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
